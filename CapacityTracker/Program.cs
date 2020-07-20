1225e48ec8bf2e6f998a81c188f723da5e65fff1
Danghor <danghor+github.com@gmail.com>
2020-07-20 18:15:30 +0200

    Make interval and url configurable and save to file

--- CapacityTracker/Program.cs
+++ CapacityTracker/Program.cs
@@ -1,28 +1,43 @@
-﻿using HtmlAgilityPack;
+﻿using CapacityTracker.Properties;
+using HtmlAgilityPack;
 using System;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading;
 
 namespace CapacityTracker
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("DE-de");
 
             Console.WriteLine("This program periodically fetches the information and saves it into a csv file. Press Ctrl-C to quit.");
 
             var exitEvent = new ManualResetEvent(false);
-            Console.CancelKeyPress += (sender, eventArgs) => {
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
                 eventArgs.Cancel = true;
                 exitEvent.Set();
             };
 
-            Schedule(TimeSpan.FromSeconds(5), () => {
-                Console.WriteLine($"{DateTime.Now} - {GetCurrentLoad()}");
+            Schedule(TimeSpan.FromSeconds(int.Parse(Resources.FetchIntervalInSeconds)), () =>
+            {
+                string line;
+
+                try
+                {
+                    line = $"{DateTime.Now}, {GetCurrentLoad()}";
+                }
+                catch (Exception ex)
+                {
+                    line = $"{DateTime.Now} ERROR: Could not fetch load: {ex.Message}";
+                }
+
+                File.AppendAllLines(Resources.OutputFile, new[] { line });
             });
 
             exitEvent.WaitOne();
@@ -32,6 +47,7 @@ namespace CapacityTracker
 
         private static void Schedule(TimeSpan interval, Action action)
         {
+            action();
             var timer = new System.Timers.Timer(interval.TotalMilliseconds);
             timer.Elapsed += (o, e) => action();
             timer.Start();
@@ -40,7 +56,8 @@ namespace CapacityTracker
         private static int GetCurrentLoad()
         {
             var htmlWeb = new HtmlWeb();
-            var htmlDocument = htmlWeb.Load("https://www.boulderwelt-frankfurt.de/");
+
+            var htmlDocument = htmlWeb.Load(Resources.Url);
 
             var loadNode = htmlDocument.DocumentNode.SelectNodes("/html/body/div[1]/div[1]/header/div[2]/a/div[2]/div[3]/div");
             var load = loadNode.Single().InnerText;
