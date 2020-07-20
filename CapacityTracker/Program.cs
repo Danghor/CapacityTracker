87e0cc1c5d9ea12b27ecb8cfbd67d063d1ba0919
Danghor <danghor+github.com@gmail.com>
2020-07-20 17:28:25 +0200

    Add project files.

---
+++ CapacityTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CapacityTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
